6caba2be5d3c7840dd3adb30aba69b18c816b962
shxblx <shiblibasheer27@gmail.com>
2024-04-06 15:17:04 +0530

    scroll bar done

---
+++ src/components/Row.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
+const Row = ({ title, fetchURL }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [fetchURL]);
+
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex items-center">
+        <MdChevronLeft />
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
+          {movies.map((item, id) => (
+            <Movie key={id} item={item} />
+          ))}
+        </div>
+        <MdChevronRight />
+      </div>
+    </>
+  );
+};
+
+export default Row;
